b3c669768322c2dfadae2deab3495542b6427ba0
Leonardo Martins <leomartinsgc@gmail.com>
2024-12-13 09:44:41 -0300

    :sparkles: cors aceitando tudo

--- src/main/java/finger/jetty/AppConfig.java
+++ src/main/java/finger/jetty/AppConfig.java
@@ -23,13 +23,7 @@ public class AppConfig {
     @Bean
     public CorsConfigurationSource corsConfiguration() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-        		"http://refeicao.localhost:3000", 
-        		"http://localhost:8080", 
-        		"http://refeicao.localhost:8080", 
-        		"http://10.1.4.220:4000", 
-        		"http://10.1.4.220:3000",
-        		"http://refeicao.tsaquimica.com.br"));
+        corsConfig.addAllowedOriginPattern("*");
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         corsConfig.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "Pragma", "Cache-Control"));
         corsConfig.setExposedHeaders(Arrays.asList("X-Custom-Header"));
